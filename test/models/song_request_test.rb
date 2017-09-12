b708dc41e4f4d340eab32aa5c33ccc1652358fa5
Jordan Hudgens <jordan@wow.com>
2017-09-11 17:30:04 -0700

    Built out the fixtures and tests for the song request system

--- test/models/song_request_test.rb
+++ test/models/song_request_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class SongRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "That a song belongs to a user" do
+    assert song_requests(:shake_it_off).user == users(:jaime_lannister)
+  end
+
+  test "That a song belongs to a location" do
+    assert song_requests(:shake_it_off).location == locations(:old_town)
+  end
 end
